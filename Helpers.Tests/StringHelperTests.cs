fcb85f1766cd5449caa631c8c107f6dc2c9a923a
hamitdogan17 <hamitdogan17@gmail.com>
2020-06-15 09:51:39 +0300

    initial commit

---
+++ Helpers.Tests/StringHelperTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Helpers.Tests
+{
+    [TestClass]
+    public class StringHelperTests
+    {
+        [TestMethod]
+        public void Girilen_ifadenin_basındaki_ve_sonundaki_bosluklar_silinmelidir()
+        {
+            //Arrange
+            var ifade = "  Hamit Dogan    ";
+            var beklenen = "Hamit Dogan";
+
+            //Act
+            var gerceklesen = StringHelper.FazlaBosluklariSil(ifade);
+
+            //Assert 
+            Assert.AreEqual(beklenen, gerceklesen);
+        }
+        [TestMethod]
+        public void Girilen_ifadenin_icindeki_fazla_bosluklar_varsa_silinmelidir()
+        {
+            //Arrange
+            var ifade = "Hamit     Dogan";
+            var beklenen = "Hamit Dogan";
+
+            //Act
+            var gerceklesen = StringHelper.FazlaBosluklariSil(ifade);
+
+            //Assert 
+            Assert.AreEqual(beklenen, gerceklesen);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CheckHasError()
+        {
+            StringHelper.ThrowExceptions();
+        }
+    }
+}
---
+++ Helpers.Tests/TestBase.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Helpers.Tests
+{
+    [TestClass]
+    public class TestBase
+    {
+        [AssemblyInitialize]
+        public static void AssemblyInitialize(TestContext testContext)
+        {
+            Debug.WriteLine("Running AssemblyInitialize");
+        }
+        [AssemblyCleanup]
+        public static void AssemblyCleanup()
+        {
+            Debug.WriteLine("Running AssemblyCleanup");
+        }
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            Debug.WriteLine("Running ClassInitialize");
+        }
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            Debug.WriteLine("Running ClassCleanup");
+        }
+        [TestInitialize]
+        public static void TestInitialize()
+        {
+            Debug.WriteLine("Running TestInitialize");
+        }
+        [TestCleanup]
+        public static void TestCleanup()
+        {
+            Debug.WriteLine("Running TestCleanup");
+        }
+
+        [TestMethod]
+        public static void TestMethod1()
+        {
+            Debug.WriteLine("Running TestMethod1");
+        }
+        [TestMethod]
+        public static void TestMethod2()
+        {
+            Debug.WriteLine("Running TestMethod2");
+        }
+    }
+}
